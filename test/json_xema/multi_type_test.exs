6501f108afebaac82bd67b8ea5f01bdc632f6221
Marcus Kruse <esurk.sucram@gmail.com>
2018-05-10 12:36:31 +0200

    Add tests

---
+++ test/json_xema/multi_type_test.exs
@@ -0,0 +1,59 @@
+defmodule Xema.MultiTypeTest do
+  use ExUnit.Case, async: true
+
+  import Xema, only: [validate: 2]
+
+  test "&new/1 called with a wrong type list raised an exception" do
+    assert_raise ArgumentError, fn ->
+      JsonXema.new(~s({"type": ["string", "foo"]}))
+    end
+  end
+
+  describe "schema with type string or nil:" do
+    setup do
+      %{schema: JsonXema.new(~s({
+        "type": ["string", "null"],
+        "minLength": 5
+      }))}
+    end
+
+    test "with a string", %{schema: schema} do
+      assert validate(schema, "foobar") == :ok
+    end
+
+    test "with an invalid string", %{schema: schema} do
+      assert validate(schema, "foo") == {:error, %{min_length: 5, value: "foo"}}
+    end
+
+    test "with nil", %{schema: schema} do
+      assert validate(schema, nil) == :ok
+    end
+
+    test "with integer", %{schema: schema} do
+      assert validate(schema, 42) == {:error, %{type: ["string", "null"], value: 42}}
+    end
+  end
+
+  describe "property with type number or nil:" do
+    setup do
+      %{schema: JsonXema.new(~s({
+        "properties": {
+          "foo": ["number", "null"]
+        }
+      }))}
+    end
+
+    test "with a number", %{schema: schema} do
+      assert validate(schema, %{foo: 42}) == :ok
+    end
+
+    test "with nil", %{schema: schema} do
+      assert validate(schema, %{foo: nil}) == :ok
+    end
+
+    test "with a string", %{schema: schema} do
+      assert validate(schema, %{foo: "foo"}) ==
+               {:error, %{properties: %{foo: %{type: [:number, nil], value: "foo"}}}}
+    end
+  end
+end
---
+++ test/json_xema/nested_test.exs
@@ -0,0 +1,80 @@
+defmodule Xema.NestedTest do
+  use ExUnit.Case, async: true
+
+  import JsonXema, only: [validate: 2]
+
+  describe "list of objects in one schema:" do
+    setup do
+      %{
+        schema: JsonXema.new(~s(
+            {
+              "type": "object",
+              "properties": {
+                "id": {
+                  "type": "number",
+                  "minimum": 1
+                },
+                "items": {
+                  "type": "array",
+                  "items": {
+                    "type": "object",
+                    "properties": {
+                      "num": {
+                        "type": "number",
+                        "minimum": 0
+                      },
+                      "desc": {
+                        "type": "string"
+                      }
+                    }
+                  }
+                }
+              }
+            }
+          ))
+      }
+    end
+
+    test "validate/2 with valid data", %{schema: schema} do
+      data = %{
+        id: 5,
+        items: [
+          %{num: 1, desc: "foo"},
+          %{num: 2, desc: "bar"}
+        ]
+      }
+
+      assert validate(schema, data) == :ok
+    end
+
+    test "validate/2 with invalid data", %{schema: schema} do
+      data = %{
+        id: 5,
+        items: [
+          %{num: 1, desc: "foo"},
+          %{num: -2, desc: "bar"}
+        ]
+      }
+
+      error = {
+        :error,
+        %{
+          properties: %{
+            items: [
+              {
+                1,
+                %{
+                  properties: %{
+                    num: %{value: -2, minimum: 0}
+                  }
+                }
+              }
+            ]
+          }
+        }
+      }
+
+      assert validate(schema, data) == error
+    end
+  end
+end
