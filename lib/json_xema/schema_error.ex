3a64207212f90e38a7392173373444de9dd0d709
Marcus Kruse <esurk.sucram@gmail.com>
2018-12-09 21:43:10 +0100

    Add schema validator

---
+++ lib/json_xema/schema_error.ex
@@ -0,0 +1,22 @@
+defmodule JsonXema.SchemaError do
+  @moduledoc """
+  Raised when a schema can't be build.
+  """
+
+  alias JsonXema.SchemaError
+
+  defexception [:message, :reason]
+
+  @impl true
+  def exception(message) when is_binary(message),
+    do: %SchemaError{message: message, reason: nil}
+
+  def exception(reason),
+    do: %SchemaError{
+      message:
+        "Can't build schema! Reason:\n#{
+          reason |> inspect() |> Code.format_string!()
+        }",
+      reason: reason
+    }
+end
