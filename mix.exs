cfdb9f473fd4f4874386b9152011de1b1b901dd6
Marcus Kruse <esurk.sucram@gmail.com>
2019-01-15 20:05:06 +0100

    Update version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule JsonXema.MixProject do
   def project do
     [
       app: :json_xema,
-      version: "0.1.3",
+      version: "0.1.4",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
