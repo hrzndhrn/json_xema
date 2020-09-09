4475fb74147c3f2943317e04c8b03a2df922bd11
Marcus Kruse <esurk.sucram@gmail.com>
2020-09-09 20:17:21 +0200

    Update test suite

---
+++ test/json_schema_test_suite/draft6/optional/format/uri_template_test.exs
@@ -0,0 +1,27 @@
+defmodule JsonSchemaTestSuite.Draft6.Optional.Format.UriTemplateTest do
+  use ExUnit.Case
+
+  import JsonXema, only: [valid?: 2]
+
+  describe ~s|format: uri-template| do
+    setup do
+      %{schema: JsonXema.new(%{"format" => "uri-template"})}
+    end
+
+    test ~s|a valid uri-template|, %{schema: schema} do
+      assert valid?(schema, "http://example.com/dictionary/{term:1}/{term}")
+    end
+
+    test ~s|an invalid uri-template|, %{schema: schema} do
+      refute valid?(schema, "http://example.com/dictionary/{term:1}/{term")
+    end
+
+    test ~s|a valid uri-template without variables|, %{schema: schema} do
+      assert valid?(schema, "http://example.com/dictionary")
+    end
+
+    test ~s|a valid relative uri-template|, %{schema: schema} do
+      assert valid?(schema, "dictionary/{term:1}/{term}")
+    end
+  end
+end
